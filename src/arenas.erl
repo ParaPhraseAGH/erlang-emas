1133f62aa593e37c536a85b497d2bc658e214196
jasiek <jasieek@student.agh.edu.pl>
2013-08-22 14:01:17 +0200

    Scalenie wszystkich modeli w jeden projekt (2)

---
+++ src/arenas.erl
@@ -0,0 +1,156 @@
+%% @author jstypka <jasieek@student.agh.edu.pl>
+%% @version 1.0
+
+-module(arenas).
+-export([startBar/3, startRing/1, startPort/2]).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+
+%% @spec start(SupervisorPid) -> ok
+%% @doc Funkcja startujaca ring.
+startRing(Supervisor) ->
+  random:seed(erlang:now()),
+  ring(Supervisor,[]).
+
+%% @spec start(SupervisorPid,KingPid) -> ok | timeout
+%% @doc Funkcja startujaca port. Na poczatku wysylane jest zapytanie do
+%% krola o liste Aren, a pozniej nastepuje czekanie na odpowiedz.
+startPort(Supervisor,King) ->
+  random:seed(erlang:now()),
+  Ref = erlang:monitor(process, King),
+  King ! {self(),Ref,getArenas},
+  receive
+    {Ref,Arenas} ->
+      erlang:demonitor(Ref, [flush]),
+      port(Supervisor,Arenas);
+    {'DOWN', Ref, process, King, Reason} ->
+      io:format("The king is dead, long live the king!~n",[]),
+      erlang:error(Reason)
+  after 1000 ->
+    io:format("Port ~p nie dostal wiadomosci z arenami~n",[self()]),
+    timeout
+  end.
+
+%% @spec start(SupervisorPid,RingPid,PortPid) -> ok
+%% @doc Funkcja startujaca bar.
+startBar(Supervisor,Ring,Port) ->
+  random:seed(erlang:now()),
+  bar(Supervisor,[],Ring,Port).
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+%% @spec receiver(SupervisorPid,List1,RingPid,PortPid) -> ok
+%% @doc Glowna funkcja baru odpowiadajaca na wiadomosci. Bar moze otrzymac
+%% zgloszenie do krzyzowania/mutacji lub sygnal zamkniecia od supervisora.
+%% List1 jest lista agentow oczekujacych na pojawienie sie wystarczajacej
+%% liczby osobnikow w barze do rozpoczecia reprodukcji.
+bar(Supervisor,Waitline,Ring,Port) ->
+  receive
+    {Pid1,Ref1,Agent1} -> %{Solution,Fitness,Energy} = Agent,
+      case Waitline of
+        [] -> bar(Supervisor,[{Pid1,Ref1,Agent1}],Ring,Port);
+        [{Pid2,Ref2,Agent2}] ->
+          [{_,_,NewEnergy1},{_,_,NewEnergy2},NewAgent1,NewAgent2] = evolution:doReproduce({Agent1,Agent2}),
+          report(Supervisor,Agent1,Agent2,NewAgent1,NewAgent2),
+          Pid1 ! {Ref1,NewEnergy1},
+          Pid2 ! {Ref2,NewEnergy2},
+          spawn(agent,start,[Ring,self(),Port,NewAgent1]),
+          spawn(agent,start,[Ring,self(),Port,NewAgent2]),
+          bar(Supervisor,[],Ring,Port)
+      end;
+    {finish,Supervisor} ->
+      answer([{Pid,Ref,0,0} || {Pid,Ref,_} <- Waitline]),
+      cleaner(Supervisor);
+    _ ->
+      io:format("Bar ~p dostal cos dziwnego~n",[self()])
+  after config:arenaTimeout() ->
+    case Waitline of
+      [] -> nothing;
+      [{Pid,Ref,Agent}] ->
+        io:format("Bar ~p reprodukuje pojedynczego osobnika!~n",[self()]),
+        [{_,_,NewEnergy},NewAgent] = evolution:doReproduce({Agent}),
+        Pid ! {Ref,NewEnergy},
+        spawn(agent,start,[Ring,self(),Port,NewAgent]),
+        bar(Supervisor,[],Ring,Port)
+    end
+  end.
+
+%% @spec receiver(SupervisorPid,List1) -> ok
+%% @doc Glowna funkcja ringu odpowiadajaca na wiadomosci. Ring moze otrzymac
+%% zgloszenie do walki lub sygnal zamkniecia od supervisora.
+%% List1 jest lista agentow oczekujacych na pojawienie sie wystarczajacej
+%% liczby osobnikow w ringu do rozpoczecia walk.
+ring(Supervisor,Waitline) ->
+  receive
+    {Pid,Ref,{Fitness,Energy}} ->
+      Agent = {Pid,Ref,Fitness,Energy},
+      case length(Waitline) == config:fightNumber() - 1 of
+        false -> ring(Supervisor,[Agent|Waitline]);
+        true ->
+          NewAgents = evolution:eachFightsAll([Agent|Waitline]),
+          answer(NewAgents),
+          ring(Supervisor,[])
+      end;
+    {finish,Supervisor} ->
+      answer([{P,R,0,0} || {P,R,_,_} <- Waitline]), % wyslij wiadomosc konczaca rowniez do procesow w waitline
+      cleaner(Supervisor);
+    _ ->
+      io:format("Ring ~p dostal cos dziwnego~n",[self()])
+  after config:arenaTimeout() ->
+    case length(Waitline) of
+      0 -> nothing;
+      _ ->
+        io:format("Ring ~p daje do walki niepelna liczbe osobnikow!~n",[self()]),
+        Agents = evolution:eachFightsAll([Waitline]),
+        answer(lists:flatten(Agents)),
+        ring(Supervisor,[])
+    end
+  end.
+
+%% @spec receiver(SupervisorPid,Arenas) -> ok
+%% @doc Funkcja glowna portu. Oczekuje na wiadomosci i albo umozliwia
+%% migracje albo konczy prace portu.
+port(Supervisor,Arenas) ->
+  receive
+    {Pid, Ref, emigration} ->
+      Index = random:uniform(length(Arenas)),
+      NewArenas = lists:nth(Index,Arenas),
+      Pid ! {Ref,NewArenas},
+      port(Supervisor,Arenas);
+    {finish,Supervisor} ->
+      cleaner(Supervisor)
+  end.
+
+%% @spec answer(AgentList) -> ok
+%% @doc Funkcja wysyla wiadomosci do wszystkich agentow w agent list
+%% o ich energii.
+answer([]) -> ok;
+answer([{Pid,Ref,_,Energy}|Tail]) ->
+  Pid ! {Ref,Energy},
+  answer(Tail).
+
+%% @spec report(Pid,A1,A2,A3,A4) -> ok
+%% @doc Funkcja wysylajaca pod podany Pid najlepszy fitness z podanych agentow.
+report(Pid,{_,F1,_},{_,F2,_},{_,F3,_},{_,F4,_}) ->
+  Pid ! {result,lists:max([F1,F2,F3,F4])}.
+
+%% @spec cleaner(SupervisorPid) -> ok
+%% @doc Funkcja uruchamiana pod koniec zycia przez areny, odsylajaca na
+%% wszystkie zgloszenia sygnal zabijajacy. Dzieki temu gina wszyscy agenci
+%% w systemie. Po zakonczeniu funkcji zakonczony jest proces
+cleaner(Supervisor) ->
+  receive
+    {Pid,_Ref,emigration} ->
+      exit(Pid,finished),
+      cleaner(Supervisor);
+    {Pid,Ref,_} ->
+      Pid ! {Ref,0},
+      cleaner(Supervisor)
+  after config:arenaTimeout() ->
+    Supervisor ! {finished,self()},
+    exit(normal)
+  end.
---
+++ src/conc_island.erl
@@ -0,0 +1,75 @@
+%% @author jstypka <jasieek@student.agh.edu.pl>
+%% @version 1.0
+%% @doc Modul odpowiedzialny za logike pojedynczej wyspy.
+
+-module(conc_island).
+-export([run/3]).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+
+%% @spec run(Pid) -> ok
+%% @doc Funkcja uruchamiajaca supervisora dla wyspy. Argumentem jest pid
+%% tzw. krola, czyli procesu spawnujacego wyspy i czekajacego na wynik (zwykle shell).
+%% Funkcja spawnuje areny i agentow, czeka na wiadomosci oraz odsyla
+%% koncowy wynik do krola. Na koniec nastepuje zamkniecie aren i sprzatanie.
+run(King,N,Instance) ->
+  Ring = spawn(arenas,startRing,[self()]),
+  Port = spawn(arenas,startPort,[self(),King]),
+  Bar = spawn(arenas,startBar,[self(),Ring,Port]),
+  Arenas = [Ring,Bar,Port],
+  King ! {arenas,self(),Arenas}, % wysylamy adresy aren do krola, zeby mogl odeslac je portom
+  [spawn(agent,start,Arenas) || _ <- lists:seq(1,config:populationSize())],
+  FDs = io_util:prepareWriting(Instance ++ "\\" ++ integer_to_list(N)),
+  receiver(0,-99999,FDs), % obliczanie wyniku
+  Bar ! Ring ! Port ! {finish,self()},
+  io_util:closeFiles(FDs),
+  allDead = cleaner(Arenas).
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+%% @spec receiver(int) -> float()
+%% @doc Funkcja odbierajaca wiadomosci. Moga to byc meldunki o wyniku
+%% od baru lub rozkaz zamkniecia wyspy od krola. Argumentem jest licznik
+%% odliczajacy kroki do wypisywania, a zwracany jest koncowy wynik.
+receiver(Counter,Best,FDs) ->
+  receive
+    {result,Result} ->
+      Step = config:printStep(),
+      if Counter == Step ->
+        io:format("Fitness: ~p~n",[Result]),
+        NewCounter = 0;
+      Counter /= Step ->
+        NewCounter = Counter + 1
+      end,
+      if Best > Result ->
+        receiver(NewCounter,Best,FDs);
+      Best =< Result ->
+        io_util:write(dict:fetch(fitness,FDs),Result),
+        receiver(NewCounter,Result,FDs)
+      end;
+    close ->
+      forcedShutdown
+  after config:supervisorTimeout() ->
+    io:format("Timeout na wyspie ~p~n",[self()]),
+    timeout
+  end.
+
+%% @spec cleaner(List1) -> allDead | notAllDead
+%% @doc Funkcja upewnia sie, ze wszystkie areny z listy przeslanej
+%% w argumencie koncza poprawnie swoje dzialanie.
+cleaner([]) ->
+  allDead;
+cleaner(Arenas) ->
+  receive
+    {finished,Pid} ->
+      true = lists:member(Pid,Arenas), %debug
+      cleaner(lists:delete(Pid,Arenas));
+    {result,_} ->
+      cleaner(Arenas)
+  after config:supervisorTimeout() ->
+    notAllDead
+  end.
