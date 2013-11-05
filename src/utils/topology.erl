f2b73e03baa4dae94da99ef49860f833fc63b363
jstypka <jasieek@student.agh.edu.pl>
2013-11-05 11:53:46 +0100

    specs + indents

--- src/utils/topology.erl
+++ src/utils/topology.erl
@@ -9,7 +9,7 @@
 -export([start_link/2, getDestination/1, close/0]).
 %% gen_server
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
-  code_change/3]).
+         code_change/3]).
 
 -type topology() :: mesh | ring.
 
@@ -18,27 +18,35 @@
 %% ====================================================================
 -spec start_link(integer(),topology()) -> {ok,pid()}.
 start_link(IslandsNr,Topology) ->
-  gen_server:start_link({local, ?MODULE}, ?MODULE, [IslandsNr,Topology], []).
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [IslandsNr,Topology], []).
 
 -spec close() -> ok.
 close() ->
-  gen_server:cast(whereis(?MODULE),close).
+    gen_server:cast(whereis(?MODULE),close).
 
 -spec getDestination(integer()) -> integer().
 %% @doc Funkcja komunikujaca sie synchronicznie z procesem topology i zwracajaca docelowa wyspe
 %%  na podstawie przeslanego argumentu
 getDestination(X) ->
-  gen_server:call(whereis(?MODULE),{destination,X}).
+    gen_server:call(whereis(?MODULE),{destination,X}).
 
 %% ====================================================================
 %% Callbacks
 %% ====================================================================
 -record(state, {n,topology}).
+-type state() :: #state{}.
 
+-spec init(term()) -> {ok,state()}.
 init([IslandsNr,Topology]) ->
-  misc_util:seedRandom(),
-  {ok, #state{n = IslandsNr, topology = Topology}}.
+    misc_util:seedRandom(),
+    {ok, #state{n = IslandsNr, topology = Topology}}.
 
+-spec handle_call(term(),{pid(),term()},state()) -> {reply,term(),state()} |
+                                                    {reply,term(),state(),hibernate | infinity | non_neg_integer()} |
+                                                    {noreply,state()} |
+                                                    {noreply,state(),hibernate | infinity | non_neg_integer()} |
+                                                    {stop,term(),term(),state()} |
+                                                    {stop,term(),state()}.
 handle_call({destination, X}, _From, State) ->
     Ans = case State#state.n of
               1 -> 1;
@@ -62,20 +70,29 @@ handle_call({destination, X}, _From, State) ->
           end,
     {reply, Ans, State}.
 
+-spec randomMesh(non_neg_integer(),non_neg_integer()) -> non_neg_integer().
 randomMesh(N,From) ->
     Destination = random:uniform(N),
     if Destination == From -> randomMesh(N,From);
-        Destination =/= From -> Destination
+       Destination =/= From -> Destination
     end.
 
+-spec handle_cast(term(),state()) -> {noreply,state()} |
+                                     {noreply,state(),hibernate | infinity | non_neg_integer()} |
+                                     {stop,term(),state()}.
 handle_cast(close, State) ->
-  {stop, normal, State}.
+    {stop, normal, State}.
 
+-spec handle_info(term(),state()) -> {noreply,state()} |
+                                     {noreply,state(),hibernate | infinity | non_neg_integer()} |
+                                     {stop,term(),state()}.
 handle_info(_Request, State) ->
-  {noreply, State}.
+    {noreply, State}.
 
+-spec terminate(term(),state()) -> no_return().
 terminate(_Reason, _State) ->
-  ok.
+    ok.
 
+-spec code_change(term(),state(),term()) -> {ok, state()}.
 code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
+    {ok, State}.
