39c7ac245bc0d5e67223dd8e4a802ea448d902ab
jstypka <jasieek@student.agh.edu.pl>
2013-11-14 16:04:15 +0100

    specs

--- src/utils/topology.erl
+++ src/utils/topology.erl
@@ -9,7 +9,7 @@
 -export([start_link/2, getDestination/1, close/0]).
 %% gen_server
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
-  code_change/3]).
+         code_change/3]).
 
 -type topology() :: mesh | ring.
 
@@ -18,17 +18,17 @@
 %% ====================================================================
 -spec start_link(integer(),topology()) -> {ok,pid()}.
 start_link(IslandsNr,Topology) ->
-  gen_server:start_link({local, ?MODULE}, ?MODULE, [IslandsNr,Topology], []).
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [IslandsNr,Topology], []).
 
 -spec close() -> ok.
 close() ->
-  gen_server:cast(whereis(?MODULE),close).
+    gen_server:cast(whereis(?MODULE),close).
 
 -spec getDestination(integer()) -> integer().
 %% @doc Funkcja komunikujaca sie synchronicznie z procesem topology i zwracajaca docelowa wyspe
 %%  na podstawie przeslanego argumentu
 getDestination(X) ->
-  gen_server:call(whereis(?MODULE),{destination,X}).
+    gen_server:call(whereis(?MODULE),{destination,X}).
 
 %% ====================================================================
 %% Callbacks
@@ -36,8 +36,8 @@ getDestination(X) ->
 -record(state, {n,topology}).
 
 init([IslandsNr,Topology]) ->
-  misc_util:seedRandom(),
-  {ok, #state{n = IslandsNr, topology = Topology}}.
+    misc_util:seedRandom(),
+    {ok, #state{n = IslandsNr, topology = Topology}}.
 
 handle_call({destination, X}, _From, State) ->
     Ans = case State#state.n of
@@ -64,14 +64,17 @@ handle_call({destination, X}, _From, State) ->
           end,
     {reply, Ans, State}.
 
+-spec handle_cast(term(),state()) -> {noreply,state()} |
+                                     {noreply,state(),hibernate | infinity | non_neg_integer()} |
+                                     {stop,term(),state()}.
 handle_cast(close, State) ->
-  {stop, normal, State}.
+    {stop, normal, State}.
 
 handle_info(_Request, State) ->
-  {noreply, State}.
+    {noreply, State}.
 
 terminate(_Reason, _State) ->
-  ok.
+    ok.
 
 code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
+    {ok, State}.
