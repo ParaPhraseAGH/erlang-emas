c8b353246f39e5877e7a2ebe9833e3ecf2fcbaad
Daniel Krzywicki <daniel.krzywicki@agh.edu.pl>
2013-11-18 16:30:47 +0100

    Remove old scripts

--- scripts/zeus/extractor.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-#PBS -A plgkrzywic2013b 
-#PBS -N emas-xtrct
-#PBS -k n
-#PBS -l mem=1024mb
-#PBS -l nodes=1:ppn=1
-
-java -jar $EmasRoot/stat-extractor.jar $outputPath fitnessMax:avg populationSum:avg
