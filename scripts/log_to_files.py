6a88c2de050be123d4e986133e47f524fbde5071
Piotr Anielski <anielski@student.agh.edu.pl>
2013-12-04 15:21:23 +0100

    zgadywanie, czy logi sa w starym czy nowym formacie i dostosowanie istniejacych skryptow

--- scripts/log_to_files.py
+++ scripts/log_to_files.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 
+'''
+this script is a tool to extract logs from stdout 
+and save the data to separate files (death.txt, fitness.txt, etc.)
+'''
+
 import os
 import sys
 from collections import defaultdict
-
 
 def read_zeus_lines(filepath):
     with open(filepath) as f:
         return [line.strip() for line in f]
 
+def is_format_old(lines):
+    empty_lines = len([e for e in lines if not e])
+    ratio = float(empty_lines)/len(lines)
+    # print 'empty_lines:', empty_lines, '/', len(lines), '=',
+    # print "%.2f" % (ratio)
+    if ratio < 0.1: # empirical
+        return False
+    return True
 
 def write_stats_to_file(directory, attr, data):
     if not os.path.exists(directory):
@@ -18,30 +30,37 @@ def write_stats_to_file(directory, attr, data):
         for line in data:
             f.write(str(line) + '\n')
 
-def parse(logfile, out_dir):
-    
+def parse(logfile, out_dir, old_format=False):
     lines = read_zeus_lines(logfile)
+    old_format = is_format_old(lines)
     attrs1 = ['fitness', 'population']
     attrs2 = ['fight', 'reproduction', 'death', 'migration']
-    
-    for attr in attrs1:
-        isl_dict = defaultdict(list)
-        data = [line.split(' ') for line in lines if line.startswith(attr)]
-        for line in data:
-            isl_dict[line[1]].append(float(line[2]))
-        vals = zip(*isl_dict.values())
-        if attr == 'fitness':
-            data2 = [max(tup) for tup in vals]
-        else:
-            data2 = [sum(tup) for tup in vals]
-        if data2:
-            write_stats_to_file(out_dir, attr, data2)
+    if old_format:
+        attrs = attrs1 + attrs2
+        for attr in attrs:
+            data = [float(line.split(':')[1].strip()) for line in lines if line.startswith(attr)]
+            # print attr, data
+            if data:
+                write_stats_to_file(out_dir, attr, data)
+    else:
+        for attr in attrs1:
+            isl_dict = defaultdict(list)
+            data = [line.split(' ') for line in lines if line.startswith(attr)]
+            for line in data:
+                isl_dict[line[1]].append(float(line[2]))
+            vals = zip(*isl_dict.values())
+            if attr == 'fitness':
+                data2 = [max(tup) for tup in vals]
+            elif attr == 'population':
+                data2 = [sum(tup) for tup in vals]
+            if data2:
+                write_stats_to_file(out_dir, attr, data2)
 
-    for attr in attrs2:
-        data = [float(line.split(' ')[-1].strip()) for line in lines if line.startswith(attr)]
-        # print attr, data
-        if data:
-            write_stats_to_file(out_dir, attr, data)
+        for attr in attrs2:
+            data = [float(line.split(' ')[-1].strip()) for line in lines if line.startswith(attr)]
+            # print attr, data
+            if data:
+                write_stats_to_file(out_dir, attr, data)
 
 
 if __name__ == '__main__':
