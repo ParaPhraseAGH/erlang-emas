2fabc42d7e678f7f023ff458eabfef2ef029537a
Piotr Anielski <plganiel@zeus.cyfronet.pl>
2014-02-17 14:11:58 +0100

    aktualny skrypt uruchamiajacy na zeusie

--- scripts/zeus/run.sh
+++ scripts/zeus/run.sh
@@ -5,29 +5,31 @@ OutputRoot=output
 
 # Computation settings
 N=1			# Number of runs for each computation
-Time=6000       	# Computation time [ms]
+Time=5000       	# Computation time [ms]
 Problem=100		# Problem size	
-Islands=12		# Islands amount
+Islands=4		# Islands amount
 Topology=mesh		# Islands topology
 
 # Common Zeus settings
-CommonSettings="-t 1-$N"				# Number of runs for each job
+CommonSettings="-q l_bigmem"
+CommonSettings+=" ""-t 1-$N"				# Number of runs for each job
 CommonSettings+=" ""-l mem=1gb"				# Available memory
 CommonSettings+=" ""-l file=500mb"			# Available disc
 CommonSettings+=" ""-l walltime=$(($Time / 800))"	# Available time [s] (~120% predicted computation time)
 CommonSettings+=" ""-j oe"				# Join stdout and stderr
-CommonSettings+=" ""-A plgkrzywic2013b"			# Grant ID
+CommonSettings+=" ""-A paraphrase1"			# Grant ID
 CommonSettings+=" ""-N emas"				# Job name
+#CommonSettings+=" ""-q l_bigmem"
 
 # Independent variables
-Models=(sequential_lists) # hybrid concurrent)
-Cores=(1 2 4 8 12)
+Models=(hybrid) # sequential_lists hybrid)
+Cores=(18) # 1 2 12
 
 for model in ${Models[*]}; do
     for cores in ${Cores[*]}; do
 
 	Command="module load erlang\n"
-	Command+="erl -pa $EmasRoot/ebin -noshell -run $model start $Problem $Time $Islands $Topology -run init stop"
+	Command+="erl -pa $EmasRoot/ebin -noshell -run $model start $Problem $Time $Islands $Topology standard_io -run init stop"
 
 	outputPath=$OutputRoot/$model/$cores
 	mkdir -p $outputPath
