929e323f2b38380174d3972de6eb4d583e841569
Piotr Anielski <pr.anielski@gmail.com>
2014-10-02 15:07:49 +0200

    adapted zeus run scripts to test binary ops module performance

--- scripts/zeus/run.sh
+++ scripts/zeus/run.sh
@@ -1,14 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 EmasRoot="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../.. && pwd )"
-OutputRoot=output
+OutputDir=output
+GrantID=plganiel2014a
 
 # Computation settings
 N=10			# Number of runs for each computation
-Time=900000       	# Computation time [ms]
-Problem=100		# Problem size	
-Islands=12		# Islands amount
-Topology=mesh		# Islands topology
+Time=1800000       	# Computation time [ms]
+# not configurable here
+#Problem=100		# Problem size	
+#Islands=12		# Islands amount
+#Topology=mesh		# Islands topology
+
+OpsModule=erlops
+# recompile for new ops module
+rm $EmasRoot/ebin/emas_config.beam
+./rebar compile -D $OpsModule
+
+OutputRoot=`$EmasRoot/scripts/get_params.erl $OutputDir`"_"`expr $Time / 1000`"s"
+echo $OutputRoot
 
 # Common Zeus settings
 CommonSettings="-t 1-$N"				# Number of runs for each job
@@ -16,23 +26,25 @@ CommonSettings+=" ""-l mem=1gb"				# Available memory
 CommonSettings+=" ""-l file=500mb"			# Available disc
 CommonSettings+=" ""-l walltime=$(($Time / 800))"	# Available time [s] (~120% predicted computation time)
 CommonSettings+=" ""-j oe"				# Join stdout and stderr
-CommonSettings+=" ""-A paraphrase1"			# Grant ID
+CommonSettings+=" ""-A $GrantID"			# Grant ID
 CommonSettings+=" ""-N emas"				# Job name
-CommonSettings+=" ""-q l_bigmem"
+# CommonSettings+=" ""-q l_bigmem"
 
 # Independent variables
-Models=(hybrid concurrent)
-Cores=(1 12 16)
-Procesor=opteron6276
-#Procesor=X5650
+#Models=(hybrid concurrent skel_main)
+Models=(sequential skel_main)
+Cores=(1 2 4 8 12)
+#Procesor=opteron6276
+Procesor=X5650
 
 for model in ${Models[*]}; do
     for cores in ${Cores[*]}; do
 
-	Command="module load erlang\n"
-	Command+="erl -pa $EmasRoot/ebin -noshell -run $model start $Problem $Time $Islands $Topology standard_io -run init stop"
+	Command="module load apps/erlang/17.3\n"
+	# Command+="erl -pa $EmasRoot/ebin -pa $EmasRoot/deps/*/ebin -noshell -run $model start $Problem $Time $Islands $Topology standard_io -run init stop"
+	Command+="erl -pa $EmasRoot/ebin -pa $EmasRoot/deps/*/ebin -noshell -eval 'emas:start($model,$Time).' -run init stop"	
 
-	outputPath=$OutputRoot/$model/$cores
+	outputPath=$OutputRoot/$model/$cores/$OpsModule
 	mkdir -p $outputPath
 
 	Settings=$CommonSettings		
