e961150fb8897d1bab76c0eca6536ced4ad9fd13
jstypka <jasieek@student.agh.edu.pl>
2014-10-03 16:23:39 +0200

    updated agent_env.erl with start/2 and stats/0 functions

--- src/mas/utils/agent_env.erl
+++ src/mas/utils/agent_env.erl
@@ -4,6 +4,8 @@
 
 -include ("mas.hrl").
 
+-callback start(model(), pos_integer()) -> ok.
+
 -callback initial_agent(sim_params()) -> [agent()].
 
 -callback behaviour_function(agent(), sim_params()) -> agent_behaviour().
@@ -11,3 +13,5 @@
 -callback behaviours() -> [agent_behaviour()].
 
 -callback meeting_function({agent_behaviour(), [agent()]}, sim_params()) -> [agent()].
+
+-callback stats() -> [funstat()].
