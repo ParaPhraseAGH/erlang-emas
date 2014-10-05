21d200f4e77c546b02a37341fc303c12ddbd6b03
Marcin Miszczyk <mpmiszczyk@gmail.com>
2014-10-05 19:27:48 +0200

    Make sure that skel can be run.
    Add `skel` into erlang run path.

--- run_tests.sh
+++ run_tests.sh
@@ -2,9 +2,6 @@
 
 run () {
 
-    ./rebar clean
-    ./rebar compile -D $1
-
     for run in `seq 1 $run_repeat`; do
         for model in $models; do
             for core in $cores; do
@@ -15,27 +12,32 @@ run () {
 
                 output_file=$output_root/$model/$core/$1/$logfile
                 echo $output_file
-                erl +S 4:$core -pa ebin -run emas starts $model $rtime -run init stop -noshell > $output_file
+                erl +S 4:$core -pa ebin -pa deps/*/ebin \
+                    -run emas starts $model $rtime \
+                    -run init stop -noshell   > $output_file
 
             done
         done
     done
 }
 
+
+
 output_dir="output"
 # rtime=120000
-rtime=200
+rtime=2000
 
 # cores="1 2 4"
 cores="4"
 # run_repeat=3
 run_repeat=1
-models="sequential" # "skel_main"
+models="skel_main" # "sequential"
 # models="hybrid"
 
 output_root=$output_dir/tests
 
+./rebar compile
+
 
 run nifops
-run binops
-run erlops
+
