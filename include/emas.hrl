2204ca6c50f9449120d1b63626e071fde7786c45
Marcin Miszczyk <mpmiszczyk@gmail.com>
2014-10-17 13:21:24 +0200

    Fix header problems.

---
+++ include/emas.hrl
@@ -0,0 +1,16 @@
+-include_lib("mas/include/mas.hrl").
+
+-type solution() :: [float()].
+
+-record(sim_params, {genetic_ops :: atom(),
+                     problem_size :: pos_integer(),
+                     monitor_diversity :: boolean(),
+                     initial_energy :: integer(),
+                     reproduction_threshold :: integer(),
+                     reproduction_transfer :: integer(),
+                     fight_transfer :: integer(),
+                     mutation_rate :: float(),
+                     mutation_range :: float(),
+                     mutation_chance :: float(),
+                     recombination_chance :: float(),
+                     fight_number :: pos_integer()}).
