a5d8a380a24fd42a84696dfd0e3a804ec344fc0f
Piotr Anielski <pr.anielski@gmail.com>
2014-10-21 14:30:26 +0200

    Fix dialyzer errors

--- src/emas.erl
+++ src/emas.erl
@@ -21,17 +21,17 @@
 %% Callbacks
 %% ====================================================================
 
--spec start(model(),pos_integer()) -> ok.
+-spec start(model(),pos_integer()) -> agent().
 start(Model, Time) ->
     start(Model, Time, []).
 
 
--spec start(model(),pos_integer(),[tuple()]) -> ok.
+-spec start(model(),pos_integer(),[tuple()]) -> agent().
 start(Model, Time, SimParamOptions) ->
     start(Model, Time, SimParamOptions, []).
 
 
--spec start(model(),pos_integer(),[tuple()],[tuple()]) -> ok.
+-spec start(model(),pos_integer(),[tuple()],[tuple()]) -> agent().
 start(Model, Time, SimParamOptions, ConfigOptions) ->
     SimParamsUpdated = mas_misc_util:overwrite_options(SimParamOptions, load_params()),
     Agents = mas:start(?MODULE, Model, Time, proplist_to_record(SimParamsUpdated), ConfigOptions),
@@ -39,7 +39,7 @@ start(Model, Time, SimParamOptions, ConfigOptions) ->
 
 
 %% @doc function for starting `emas` from command line
--spec starts(list()) -> ok.
+-spec starts(list()) -> agent().
 starts([Model, Time]) ->
     start(erlang:list_to_atom(Model), erlang:list_to_integer(Time)).
 
@@ -108,7 +108,7 @@ extract_best(Agents) ->
              end,
     {_Sol, _Fit, _Energy} = lists:foldl(ArgMax, hd(Agents), tl(Agents)).
 
--spec load_params() -> sim_params().
+-spec load_params() -> [{atom(), term()}].
 load_params() ->
     ConfDir = mas_misc_util:get_config_dir(),
     io:format("~p~n", [ConfDir]),
@@ -117,7 +117,7 @@ load_params() ->
     ParamsFromFile.
 
 %% @doc Transform a proplist with simulation properties to a record
--spec proplist_to_record([tuple()]) -> sim_params().
+-spec proplist_to_record([{atom(), term()}]) -> sim_params().
 proplist_to_record(Proplist) ->
     Dict = dict:from_list(Proplist),
     #sim_params{?LOAD(genetic_ops, Dict),
